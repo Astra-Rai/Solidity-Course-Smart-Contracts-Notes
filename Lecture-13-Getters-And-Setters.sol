db70066ddbe214bc809813babeb68bc66bdb16f7
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-03-19 21:09:01 -0500

    Getters-And-Setters

---
+++ Lecture-13-Getters-And-Setters.sol
@@ -0,0 +1,32 @@
+
+pragma solidity ^0.5.1.;
+
+
+//create a contract MessageContract
+
+contract MessageContract {
+    
+    //decare variable, name it MessageContract
+    //make varible private 
+    //data type of message is string
+    //define variable, data type string, set to the string "Hello World"
+    
+    string private message = "Hello Fam";
+    //getter, gets the value of      
+    function getmessage() public view returns (string memory){
+        return message;
+    }
+    
+    //function that resets value of variage message
+    function setMessage (string memory _newMessage) public {
+        //set message variable to variable _newMessage
+        message = _newMessage;
+    }    
+}
+
+
+//you can change the prefined string for the variable message
+//public view automatically have a getter function
+//setter, set the value 
+//the function setMessage is public 
+//setMessage resets value of variable message 
